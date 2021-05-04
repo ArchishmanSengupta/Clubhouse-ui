351e121201f78a0ce54754bc105c597c064ff1c3
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-05-04 22:02:02 +0530

    lobby and followers skeleton page made

--- lib/pages/welcome/home/home_page.dart
+++ lib/pages/welcome/home/home_page.dart
@@ -1,9 +1,19 @@
+import 'package:blackvector/pages/welcome/lobby/followers_page.dart';
+import 'package:blackvector/pages/welcome/lobby/lobby_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(),
+      body: PageView(
+        children:[
+          LobbyPage(),
+          FollowersPage(),
+
+        ],
+      ),
       
     );
   }
---
+++ lib/pages/welcome/lobby/lobby_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LobbyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Text('Lobby Page'),
+      ],
+      
+    );
+  }
+}
