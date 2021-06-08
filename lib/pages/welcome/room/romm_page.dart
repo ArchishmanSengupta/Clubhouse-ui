a4929c369fe49239b007a98201e97f069463e901
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-06-08 21:35:44 +0530

    room page done

--- lib/pages/welcome/room/romm_page.dart
+++ lib/pages/welcome/room/romm_page.dart
@@ -1,15 +1,20 @@
 import 'package:blackvector/models/room.dart';
+import 'package:blackvector/models/user.dart';
 import 'package:blackvector/pages/welcome/home/widgets/profile_page.dart';
 import 'package:blackvector/pages/welcome/room/widgets/room_profile.dart';
 import 'package:blackvector/util/history.dart';
+import 'package:blackvector/util/style.dart';
+import 'package:blackvector/widgets/round_button.dart';
 import 'package:blackvector/widgets/round_image.dart';
 import 'package:flutter/material.dart';
+import 'package:blackvector/util/data.dart';
 class RoomPage extends StatelessWidget {
   final Room room;
 
   const RoomPage({Key key, this.room}) : super(key: key);
 
   get myProfile => null;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,43 +23,38 @@ class RoomPage extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: Row(
           children: [
-          IconButton(
-            icon: Icon(Icons.keyboard_arrow_down,),
-            iconSize: 30,
-            onPressed: (){
-              Navigator.pop(context);
-            }
+            IconButton(
+              iconSize: 30,
+              icon: Icon(Icons.keyboard_arrow_down),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            Text('All Rooms', 
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 15,
-            ),
+            Text(
+              'All rooms',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              ),
             ),
             Spacer(),
-
-
-
-            // RoundImage(
-            //   path:  myProfile.profileImage,
-            //   width:40,
-            //   height: 40,
-
-
-            // ),
-        //     GestureDetector(
-        //       onTap:(){
-        //       History.pushPage(context, 
-        //       ProfilePage(profile: myProfile,));
-        //       },
-        //       // child: RoundImage(path: 
-        //       // myProfile.profileImage,
-        //       // width: 40,
-        //       // height: 40,
-        //       // ),
-        // ),
+            GestureDetector(
+              onTap: () {
+                History.pushPage(
+                  context,
+                  ProfilePage(
+                    profile: myProfile,
+                  ),
+                );
+              },
+              child: RoundImage(
+                //path: myProfile.profileImage,
+                width: 40,
+                height: 40,
+              ),
+            ),
           ],
-      )
+        ),
       ),
       body: Container(
         padding: const EdgeInsets.only(
@@ -67,7 +67,7 @@ class RoomPage extends StatelessWidget {
           borderRadius: BorderRadius.only(
             topRight: Radius.circular(30),
             topLeft: Radius.circular(30),
-          )
+          ),
         ),
         child: Stack(
           children: [
@@ -79,13 +79,21 @@ class RoomPage extends StatelessWidget {
               child: Column(
                 children: [
                   buildTitle(room.title),
-                  buildSpeaker(),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  buildSpeakers(room.users.sublist(0, room.speakerCount)),
+                  buildOthers(room.users.sublist(room.speakerCount)),
                 ],
-              )),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: buildBottom(context),
+            ),
           ],
-
         ),
-      )
+      ),
     );
   }
 
@@ -93,41 +101,123 @@ class RoomPage extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children:[
+      children: [
         Flexible(
-          child: Text(title, style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-
-          )),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
         Container(
           child: IconButton(
-            onPressed: (){},
-            iconSize: 20,
+            onPressed: () {},
+            iconSize: 30,
             icon: Icon(Icons.more_horiz),
           ),
-        )
-      ]
+        ),
+      ],
     );
   }
 
-  Widget buildSpeaker(){
-
+  Widget buildSpeakers(List<User> users) {
     return GridView.builder(
       shrinkWrap: true,
       physics: ScrollPhysics(),
-       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount:3,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
         mainAxisExtent: 150,
       ),
-      itemCount: room.users.length,
-      itemBuilder: (gc,index){
+      itemCount: users.length,
+      itemBuilder: (gc, index) {
         return RoomProfile(
-          user: room.users[index],
+          user: users[index],
+          isModerator: index == 0,
+          isMute: index == 3,
           size: 80,
         );
       },
+    );
+  }
+
+  Widget buildOthers(List<User> users) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            'Others in the room',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+              color: Colors.grey.withOpacity(0.6),
+            ),
+          ),
+        ),
+        GridView.builder(
+          shrinkWrap: true,
+          physics: ScrollPhysics(),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            mainAxisExtent: 100,
+          ),
+          itemCount: users.length,
+          itemBuilder: (gc, index) {
+            return RoomProfile(
+              user: users[index],
+              size: 60,
+            );
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget buildBottom(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Row(
+        children: [
+          RoundButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            color: Style.LightGrey,
+            child: Text(
+              '✌️ Leave quietly',
+              style: TextStyle(
+                color: Style.AccentRed,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Spacer(),
+          RoundButton(
+            onPressed: () {},
+            color: Style.LightGrey,
+            isCircle: true,
+            child: Icon(
+              Icons.add,
+              size: 15,
+              color: Colors.black,
+            ),
+          ),
+          RoundButton(
+            onPressed: () {},
+            color: Style.LightGrey,
+            isCircle: true,
+            child: Icon(
+              Icons.thumb_up,
+              size: 15,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
